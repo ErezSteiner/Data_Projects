9005cc5382063681ba0adaccad7bfb0b510a07f8
ErezSteiner <101408599+ErezSteiner@users.noreply.github.com>
2024-11-27 21:20:49 +0200

    Update 2 - Deep dive and analysis.sql

--- NBA_Analysis/SQL/2 - Deep dive and analysis.sql
+++ NBA_Analysis/SQL/2 - Deep dive and analysis.sql
@@ -31,15 +31,15 @@ SELECT
 FROM rnktbl AS r
 WHERE pnts_rank < 11;
 /*
-Player_ID   Player_Name             total_points        pnt_diff_rows       pnt_diff_topscr
-1629029	    Luka Doncic	                1892	        0	            0
-1628973	    Jalen Brunson	        1791	        -101	            -101
+Player_ID   Player_Name             total_points    pnt_diff_rows   pnt_diff_topscr
+1629029	    Luka Doncic	            1892	        0	            0
+1628973	    Jalen Brunson	        1791	        -101	        -101
 203999	    Nikola Jokic	        1727	        -64	            -165
 203507	    Giannis Antetokounmpo	1708	        -19	            -184
 1628983	    Shai Gilgeous-Alexander	1687	        -21	            -205
 201142	    Kevin Durant	        1670	        -17	            -222
 201939	    Stephen Curry	        1657	        -13	            -235
-1628368	    DeAaron Fox	                1654	        -3	            -238
+1628368	    DeAaron Fox	            1654	        -3	            -238
 1630162	    Anthony Edwards	        1626	        -28	            -266
 1628369	    Jayson Tatum	        1573	        -53	            -319
 */
@@ -96,35 +96,48 @@ AS
 (SELECT 
         Shot_ID,
         Action_Type,
-        COUNT(Shot_ID) OVER(PARTITION BY Action_Type) AS total_attempted,
-        SUM(CAST(Shot_Made AS float)) OVER(PARTITION BY Action_Type) AS total_made
+        CAST(COUNT(Shot_ID) OVER(PARTITION BY Action_Type) AS float) AS total_attempted,
+        CAST(SUM(CAST(Shot_Made AS int)) OVER(PARTITION BY Action_Type)AS float) AS total_made
 FROM fltrtbl AS f)
+,
+ratetbl
+AS
+(
 SELECT
         Action_Type,
-        CONCAT(FORMAT(SUM(total_made) / SUM(total_attempted) * 100, '#.##'), '%') as success_rate
+        SUM(total_made) / SUM(total_attempted) * 100 as success_rate
 FROM totaltbl AS t
-GROUP BY Action_Type;
+GROUP BY Action_Type)
+
+SELECT
+        Action_Type,
+        CONCAT(FORMAT(r.success_rate, '#.##'), '%') as success_rate
+FROM ratetbl AS r
+ORDER BY r.success_rate DESC
+
+
+
 
 /*
 Action_Type                             success_rate
+Driving Dunk Shot	                92.21%
 Alley Oop Dunk Shot	                91.71%
 Cutting Dunk Shot	                87.7%
 Cutting Layup Shot	                75.24%
-Driving Dunk Shot	                92.21%
 Driving Finger Roll Layup Shot	        67.15%
-Driving Floating Jump Shot	        49.71%
-Driving Layup Shot	                52.49%
-Fadeaway Jump Shot	                47.37%
 Floating Jump shot	                63.03%
-Hook Shot	                        61.45%
-Jump Shot	                        38.62%
-Layup Shot	                        62.5%
-Pullup Jump shot	                41.12%
-Running Jump Shot	                45.24%
 Running Layup Shot	                63.01%
-Step Back Jump shot	                40.63%
+Layup Shot	                        62.5%
+Hook Shot	                        61.45%
 Turnaround Hook Shot	                57.47%
+Driving Layup Shot	                52.49%
+Driving Floating Jump Shot	        49.71%
 Turnaround Jump Shot	                47.75%
+Fadeaway Jump Shot	                47.37%
+Running Jump Shot	                45.24%
+Pullup Jump shot	                41.12%
+Step Back Jump shot	                40.63%
+Jump Shot	                        38.62%
 */
 
 /*
@@ -139,12 +152,18 @@ This ensures that only significant shot types and players are considered.
 
 Third CTE (totaltbl):
 Calculates the total attempted shots and made shots per shot type.
-Shot_Made is stored bitwise, so it needs to be cast to float before summing to allow it to be summed
+Shot_Made is stored bitwise, so it needs to be CAST before summing to allow it to be summed(its bitwise)
 and to also provide for proper division in the subsequent CTE.
 
-Final Selection:
-Group the rows by Action_Type to calculate the success rate per action type.
+Fourth CTE(ratetbl):
+In this table I do the actual success rate calculation,
+grouping the rows by Action_Type to calculate the success rate per action type.
 This approach is more efficient than using DISTINCT,
 as it avoids unnecessary sorting and deduplication.
-Uses CONCAT and FORMAT to display the results neatly.
+I could have also formatted the result here and ordered on them doing ORDER BY 2,
+But I think an additional CTE to format the text and order on an actual number is more "correct".
+
+Final Selection:
+Uses CONCAT and FORMAT to display the results neatly,
+and then ordering the results based on the numerical value in the previous CTE.
 */
